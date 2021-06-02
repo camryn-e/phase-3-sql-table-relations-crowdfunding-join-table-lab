0572d3ad6cb9887db7bce544c76ef08327f5cb38
camryn williams <williams.camryn.e@gmail.com>
2021-06-02 14:05:58 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,68 @@
+INSERT INTO users (name, age) VALUES ("cam", 21);
+INSERT INTO users (name, age) VALUES ("alex", 23);
+INSERT INTO users (name, age) VALUES ("sam", 25);
+INSERT INTO users (name, age) VALUES ("katie", 27);
+INSERT INTO users (name, age) VALUES ("cathy", 29);
+INSERT INTO users (name, age) VALUES ("leslie", 31);
+INSERT INTO users (name, age) VALUES ("steve", 33);
+INSERT INTO users (name, age) VALUES ("bob", 35);
+INSERT INTO users (name, age) VALUES ("debbie", 37);
+INSERT INTO users (name, age) VALUES ("paul", 39);
+INSERT INTO users (name, age) VALUES ("roy", 41);
+INSERT INTO users (name, age) VALUES ("mike", 43);
+INSERT INTO users (name, age) VALUES ("janet", 45);
+INSERT INTO users (name, age) VALUES ("nicole", 47);
+INSERT INTO users (name, age) VALUES ("lindsey", 49);
+INSERT INTO users (name, age) VALUES ("jason", 51);
+INSERT INTO users (name, age) VALUES ("anna", 53);
+INSERT INTO users (name, age) VALUES ("tara", 57);
+INSERT INTO users (name, age) VALUES ("chris", 59);
+INSERT INTO users (name, age) VALUES ("barbara", 61);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("save the bees!", "environmental", 40000, "6/1/2021", "9/17/2022");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("birds ARE real", "fruitless debate with conspiracy theorists", 100, "6/1/2021", "2/30/3021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("the gay agenda <3", "pride month", 1000000, "6/1/2021", "6/30/2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("hyperloop", "unrealistic infrastructure goals", 100000000000, "6/1/2021", "1/32/2097");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("pool", "community stuff", 20000, "6/1/2021", "7/31/2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("launching all spiders into the sun", "mental health", 9000000, "6/1/2021", "6/2/2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("gov ball tickets", "i want the pandemic to end pls", 350, "6/1/2021", "9/22/2022");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("making a meme cryptocurrency", "bitcoin is too expensive", 20, "6/1/2021", "6/1/2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("removing all country music from all platforms", "world peace", 10000000, "6/1/2021", "9/17/2022");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("teleportation hubs", "realistic infrastructure goals", 4000000000, "6/1/2021", "1/3/2030");
+
+
+
+
+
+
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2, 1, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (4, 2, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (6, 3, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (8, 4, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (10, 5, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (12, 6, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (14, 7, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (16, 8, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (18, 9, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (20, 10, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (22, 11, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (24, 12, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (26, 13, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (28, 14, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (30, 15, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (32, 16, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (34, 17, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (36, 18, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (38, 19, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (40, 20, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (42, 1, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (44, 2, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (46, 3, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (48, 4, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 5, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (52, 6, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (54, 7, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (56, 8, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (58, 9, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (60, 10, 1);
